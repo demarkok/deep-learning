aa08a291ba1eee1f6d8ae06f4a93e41a73ca00ec
Ilya Kaysin <demarkok@gmail.com>
2018-09-30 23:16:13 +0300

    add trainer

--- hw02/test.py
+++ hw02/test.py
@@ -1,5 +1,6 @@
 import torch
-
+from torch.utils.data import ConcatDataset, TensorDataset
+from trainer import Trainer
 from resnext import resnext34
 
 
@@ -8,3 +9,15 @@ def test_resnext34():
     data = torch.rand(10, 3, 224, 224)
     result = net(data)
     assert result.shape == torch.Size([10, 1000])
+
+
+def test_trainer():
+    net = resnext34()
+    train_dataset = TensorDataset(torch.rand(10, 3, 224, 224), torch.zeros(10, dtype=torch.long))
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
+    loss_function = torch.nn.CrossEntropyLoss()
+    trainer = Trainer(net, optimizer, loss_function)
+    trainer.train(n_epochs=10, batch_size=1, train_data=train_dataset)
+
+
+test_trainer()
